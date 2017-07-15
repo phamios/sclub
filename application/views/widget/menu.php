9d051f3ac10e3ab292c452bc56062a453ebfaf5d
Macintosh HD <macintoshhd@Macintoshs-MacBook-Pro-4.local>
2017-07-15 13:03:23 +0700

    update

---
+++ application/views/widget/menu.php
@@ -0,0 +1,27 @@
+<div>
+    <h3>Menu</h3>
+    <nav role="navigation">
+        <ul>
+            <li id="menu-item-190" class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-190"><a href="https://www.etq-amsterdam.com/">Collection</a></li>
+            <li id="menu-item-188" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-188"><a href="https://www.etq-amsterdam.com/etq-ss17-lookbook/">Lookbook</a></li>
+            <li id="menu-item-189" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-189"><a href="https://www.etq-amsterdam.com/about/">About</a></li>
+            <li id="menu-item-187" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-187"><a href="https://www.etq-amsterdam.com/stockists/">Stockists</a></li>
+
+        </ul>
+        <ul>
+            <li id="menu-item-191" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-191"><a href="https://www.etq-amsterdam.com/service-desk/">Customer Care</a></li>
+            <li class=" menu-item menu-item-type-post_type menu-item-object-page"><a href="https://www.etq-amsterdam.com/my-account/">Login</a></li>
+        </ul>
+    </nav>
+</div>
+<footer>
+    <a href="https://www.facebook.com/etqams" target="_blanc">
+        <button class="facebook"><span><i class="fa fa-facebook"></i></span><span class="hover"><i class="fa fa-facebook"></i></span></button>
+    </a>
+    <a href="https://www.instagram.com/etqamsterdam" target="_blanc">
+        <button class="instagram"><span><i class="fa fa-instagram"></i></span><span class="hover"><i class="fa fa-instagram"></i></span></button>
+    </a>
+    <a href="https://twitter.com/ETQAmsterdam" target="_blanc">
+        <button class="twitter"><span><i class="fa fa-twitter"></i></span><span class="hover"><i class="fa fa-twitter"></i></span></button>
+    </a>
+</footer>
---
+++ application/views/widget/menu_right.php
@@ -0,0 +1,12 @@
+<aside class="cartcount show active">0<span class="label">CLOSE</span></aside>
+<div id="cart-scroll">
+    <ul class="cart-products">
+    </ul>
+</div>
+
+<footer>Total <span>€ 0.00</span></footer>
+<button data-href="https://www.etq-amsterdam.com/checkout/" class="checkout" disabled="disabled">
+    <span><strong>Checkout</strong></span>
+    <span class="hover"><strong>Checkout</strong></span>
+    <div class="spinner ng-hide"><div class="bounce1"></div><div class="bounce2"></div></div>
+</button>
