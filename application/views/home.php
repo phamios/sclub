a8e6498d97180dd808bba2d776fdd51313a6ecfb
Son Pham <sonpxvn@gmail.com>
2017-07-27 22:45:49 +0700

    update

--- application/views/home.php
+++ application/views/home.php
@@ -33,6 +33,8 @@
         document.getElementById("mySidenavRight").style.width = "0";
         document.getElementById("main").style.marginRight= "0"; 
     }
+
+  
     </script>
 
 
@@ -50,6 +52,8 @@
     <style>
     @import url('https://fonts.googleapis.com/css?family=Roboto:300,400&subset=vietnamese');
     </style>
+
+    <link rel="stylesheet" href="<?php echo base_url('templates/js/rangeslider.css') ?>">
 
   </head>
   <body>
@@ -125,7 +129,23 @@
 			todayHighlight: true,
 			autoclose: true,
 		})
-	})
+	}) 
+</script>
+
+<script src="<?php echo base_url('templates/js/rangeslider.min.js');?>"></script>
+<script>
+    // Initialize a new plugin instance for all
+    // e.g. $('input[type="range"]') elements.
+    $('input[type="range"]').rangeslider();
+
+    // Destroy all plugin instances created from the
+    // e.g. $('input[type="range"]') elements.
+    $('input[type="range"]').rangeslider('destroy');
+
+    // Update all rangeslider instances for all
+    // e.g. $('input[type="range"]') elements.
+    // Usefull if you changed some attributes e.g. `min` or `max` etc.
+    $('input[type="range"]').rangeslider('update', true);
 </script>
 
 
